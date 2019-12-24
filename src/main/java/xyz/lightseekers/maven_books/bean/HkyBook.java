7e7ef862ee92e18e891dccd7c66bd74f32350776
watermelon <476300511@qq.com>
2019-12-24 22:12:22 +0800

    何恺越的图书管理系统

--- src/main/java/xyz/lightseekers/maven_books/bean/HkyBook.java
+++ src/main/java/xyz/lightseekers/maven_books/bean/HkyBook.java
@@ -1,4 +1,47 @@
 package xyz.lightseekers.maven_books.bean;
 
+
+import io.swagger.annotations.ApiModelProperty;
+
 public class HkyBook {
+    @ApiModelProperty(value = "图书名称")
+    private String name;
+    @ApiModelProperty(value = "价格")
+    private int price;
+    @ApiModelProperty(value = "编号")
+    private int ID;
+
+    public HkyBook(String name,int price,int ID)
+    {
+        this.name = name;
+        this.price = price;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+
+
 }
--- src/main/java/xyz/lightseekers/maven_books/web/controller/HkyController.java
+++ src/main/java/xyz/lightseekers/maven_books/web/controller/HkyController.java
@@ -1,4 +1,43 @@
 package xyz.lightseekers.maven_books.web.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.lightseekers.maven_books.bean.HkyBook;
+import xyz.lightseekers.maven_books.util.Message;
+import xyz.lightseekers.maven_books.util.MessageUtil;
+
+@RestController
+@RequestMapping("/hky")
+@Api(description = "何恺越的图书管理系统")
 public class HkyController {
+     @PostMapping("/add")
+     @ApiOperation(value = "添加书籍")
+         public Message add(HkyBook hkybook)
+        {
+            return MessageUtil.success("添加成功");
+        }
+
+    @PostMapping("/del")
+    @ApiOperation(value = "删除书籍")
+    public Message del(String id)
+    {
+        return MessageUtil.success("删除成功");
+    }
+
+    @PostMapping("/update")
+    @ApiOperation(value = "更新信息")
+    public Message update(HkyBook hkybook)
+    {
+        return MessageUtil.success("更新成功");
+    }
+
+    @PostMapping("/check")
+    @ApiOperation(value = "查询书籍信息")
+    public Message search(String id)
+    {
+        return MessageUtil.success("查询书籍成功");
+    }
 }
